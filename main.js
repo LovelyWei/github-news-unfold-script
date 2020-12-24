f74129d9e831fc9bda5f9b448e04f0345b71dd9c
LovelyWei <16489620+LovelyWei@users.noreply.github.com>
2020-12-24 11:17:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Github News Unfold
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.2
+// @description  A user script that unfolds GitHub news.
 // @author       LovelyWei
 // @match        https://github.com/
 // @grant        none
 // ==/UserScript==
- 
+
 const unfold = () =>{
     const detailsContainerSelector = 'div.Details.js-details-container.js-news-feed-event-group';
     const detailsContainer = Array.from(document.querySelectorAll(detailsContainerSelector));
- 
+
     if (detailsContainer.length !== 0) {
         for (let i = 0; i < detailsContainer.length; i += 1) {
             const element = detailsContainer[i];
@@ -19,21 +19,21 @@ const unfold = () =>{
         }
     }
 }
- 
+
 const init = () => {
     const target = document.getElementsByClassName('news')[0];
- 
+
     const observer = new MutationObserver(() => {
         unfold();
     });
- 
+
     observer.observe(target, {
         attributes: true,
         childList: true,
         characterData: true,
     });
 };
- 
+
 (function() {
     'use strict';
     init();
